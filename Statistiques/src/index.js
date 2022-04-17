f1e7b50f19cc213464974324ff7135340829e436
Marilyn Themo <marilyn.themo@heig-vd.ch>
2022-04-17 21:06:23 +0200

    graphique 2

--- Statistiques/src/index.js
+++ Statistiques/src/index.js
@@ -1,6 +1,7 @@
 import "./statistiques-menages.js"
 import "./statistiques-transport.js"
-import "./statistiques-population"
+import "./statistiques-population.js"
+import "./statistiques-sommets.js"
 
 import * as d3 from 'd3';
 import { json } from 'd3-fetch' // Pour dire qu'on utilise d3
--- Statistiques/src/statistiques-transport.js
+++ Statistiques/src/statistiques-transport.js
@@ -1,20 +1,8 @@
 import * as d3 from 'd3';
 
+
+//importation des données
 import transport from '../data/dataset-transport.csv'
-// import { json } from 'd3-fetch' // Pour dire qu'on utilise d3
-
-
-// d3.json('https://ckan.opendata.swiss/api/3/action/package_show?id=viertausender')
-//     .then(function (data) {
-
-//         console.log(data)
-//     })
-//     .catch(function (error) {
-//         // Gérer l'erreur ici!
-//     })
-
-
-
 
 const tableau = transport.map((d, i) => {
     const values = {
@@ -27,19 +15,14 @@ const tableau = transport.map((d, i) => {
 
 
 
-
+//Définition des marges
 let margin = { top: 90, right: 190, bottom: 60, left: 190 };
 let width = 1200 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-
-// let width = window.innerWidth - margin.left - margin.right,
-// height = window.innerHeight - margin.top - margin.bottom;
-
 let container = d3.select("body")
     .append("div")
     .attr('id', 'graph')
-
 
 let svg = d3.select("#graph")
 
@@ -52,13 +35,10 @@ let svg = d3.select("#graph")
 
 
 
-
 //Définition des échelles 
 let x = d3.scaleBand() //echelle//Pour avoir le nom en dessous de la band (colonne) pour les données ordinales   
     .domain(tableau.map(d => d.annee))
     .range([0, width]); //Pour avoir les différents traits
-
-
 
 
 let y = d3.scaleLinear() //echelle
@@ -66,12 +46,14 @@ let y = d3.scaleLinear() //echelle
     .range([height, 0]); //Inverser l'ordre pour les données quantitatives //range doit être contenu dans le canva
 
 
+
+
 //Création des axes    
 svg.append("g") //Pour créer les axes il faut appeler les échelles correspondantes
     .attr("class", "axisY")
     .call(d3.axisLeft(y))
     .selectAll("text")
-    .style("font-size", "15px");
+    .style("font-size", "16px");
 
 
 svg.append("g") //Pour créer les axes il faut appeler les échelles correspondantes
@@ -80,27 +62,35 @@ svg.append("g") //Pour créer les axes il faut appeler les échelles corresponda
     .call(d3.axisBottom(x))
     .selectAll("text")
     .attr("transform", "translate(-2,10)") //Pour décaler les textes un peu plus bas
-    .style("font-size", "15px");
+    .style("font-size", "16px");
 
+
+//Création du graphique
 svg.selectAll("bars")
     .data(tableau)
     .enter()
     .append("rect")
     .attr("class", "rectangle")
-
     .attr('x', (d, i) => x(d.annee) + 40)
-    .attr('y', d => y(d.temps) - 0.5)
 
-    .transition()
-    .ease(d3.easeBounceOut)
-    .duration(1800)
-    .delay((d, i) => i * 70)
+    // .attr('y', d => y(d.temps) -0.3)
+    .attr("y", d => y(0)) //comme le range est inversé le 0 est maintenant en bas
 
-    .attr("height", d => y(0) - y(d.temps))
+    .attr("height", d => height - y(0)) 
+    // .attr("height", d => y(0) - y(d.temps))
+
     .attr("fill", `palevioletred`)
     .attr("width", x.bandwidth() / 2)
 
+    .transition()
+    .ease(d3.easeBounceOut)
+    .duration(3600)
+    .attr("y", d => y(d.temps) -0.9)
+    .attr("height", d => height - y(d.temps) )
+    .delay((d,i)=> i*100)
 
+
+//Labels du graphique
 svg.append('text')
     .text("années")
     .attr('text-anchor', 'end')
@@ -111,8 +101,7 @@ svg.append('text')
     .style("fill", `#black`)
     .style("font-family", `Montserrat`)
 
-
-
+//
 svg.append('text')
     .text("minutes")
     .attr('text-anchor', 'end')
@@ -123,3 +112,40 @@ svg.append('text')
     .style("text-decoration", "bold")
     .style("fill", `black`)
     .style("font-family", `Montserrat`)
+
+//
+    svg.append("text")
+    .text("Temps passé dans les transports")
+    .transition()
+    .ease(d3.easeLinear)
+    .duration(500)
+    .attr("x", 180)
+    .attr("y", 0)
+    .style("font-size", "30")
+    .style("text-decoration", "bold")
+    .style("fill", `black`)
+    .style("font-family", `Montserrat`)
+
+
+    svg.selectAll("bars")
+    .data(tableau)
+    .enter()
+    .append('text')
+    .text((d, i) => d.temps)
+    .attr('x', (d, i) => x(d.annee) + 57)
+    .attr("y", y(0))
+    .style("font-size", "0")
+
+    .transition()
+    .ease(d3.easeQuadIn)
+    .duration(1950)
+    .attr("y", 180)
+    .attr("height", d => height - y(d.temps) )
+
+    .style("font-size", "20")
+    .style("text-decoration", "bold")
+    .attr("fill", `palevioletred`)
+    .style("font-family", `Montserrat`)
+
+
+
