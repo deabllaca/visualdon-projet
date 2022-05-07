72e15075e80e255e5c0296eedfe8ae386098a7f2
Dea Bllaca <deabllaca@DeaBllaca.local>
2022-05-07 15:38:14 +0200

    paysage SVG + début du changement des sections

--- Statistiques/src/index.js
+++ Statistiques/src/index.js
@@ -8,3 +8,61 @@ import "./statistiques-loyermoyen-canton.js"
 import * as d3 from 'd3';
 import { json } from 'd3-fetch' // Pour dire qu'on utilise d3
 
+
+
+document.querySelector('#btn-transport').addEventListener('click', function (event) {
+    console.log("direction transports");
+
+    //on modifie la classe "active" sur la section
+    document.querySelector('section.active')?.classList.remove('active')
+    document.querySelector('section.stats-transport')?.classList.add('active')
+
+});
+
+document.querySelector('#btn-transport-retour').addEventListener('click', function (event) {
+    console.log("retour à la page d'acceuil!");
+
+    //on modifie la classe "active" sur la section
+    document.querySelector('section.active')?.classList.remove('active')
+    document.querySelector('section.home-section')?.classList.add('active')
+
+});
+
+
+
+
+
+
+
+function toggleSection(section) {
+    // Supprime/Ajoute la classe active sur la section
+    document.querySelector('section.active')?.classList.remove('active')
+
+}
+
+function toggleNav(section) {
+    // Supprime/Ajoute la classe active sur le lien
+    document.querySelector('circle a.active')?.classList.remove('active')
+    document.querySelector(`circle a[href="${section}"]`)?.classList.add('active')
+}
+
+// Affichage d'une section
+function displaySection() {
+    // S'il n'y a pas de hash (par ex, on est sur "localhost:8080/"), le défaut devient '#home'
+    const section = window.location.hash || '#home'
+    const sectionSplit = section.split('-')
+
+    // Toggle par défaut des sections et de la navigation
+    toggleSection(sectionSplit[0])
+    toggleNav(sectionSplit[0])
+
+    // Chargement des éléments custom par section
+    switch (sectionSplit[0]) {
+        case '#transport':
+            // On réutilise la section 'songs' en arrière plan
+            toggleSection('#stats-transport')
+            render();
+            break;
+
+    }
+}
--- Statistiques/src/statistiques-loyermoyen-canton.js
+++ Statistiques/src/statistiques-loyermoyen-canton.js
@@ -1,10 +1,12 @@
 import * as d3 from 'd3';
-
+import * as topojson from "topojson-client";
 
 
 // Population par canton en 2020
 import data from '../data/dataset-loyer-moyen-par-canton.csv'
-// import carte from '../data/swiss-maps.json'
+import carte from '../data/swiss-maps.json'
+
+
 
 // Marges et canevas
 let margin = { top: 90, right: 190, bottom: 60, left: 190 };
@@ -17,38 +19,136 @@ d3.select("body")
     .attr('id', 'graph-loyer-moyen')
 
 
-
-//map
-
-//Pour dire que l'on veut une carte
-const path = d3.geoPath();
-
-//Pour définir le centre de la carte
-const projection = d3.geoConicConformal()
-  .center([2.454071, 46.279229])
-  .scale(1000) //Pour définir la couverture
-  .translate([width / 2, height / 2]);
-
-  path.projection(projection); //associer le path à la projection
+var centered;
+var g;
 
 const svg = d3.select("#graph-loyer-moyen").append("svg")
   .attr("id", "svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
-  .attr("class", "Blues");
+
+var proj = d3.geoMercator()
+  .precision(0.1)
+  .center([138, 35])
+  //  .parallels([50, 60])
+  .rotate([0,0,0])
+  .scale(1200)
+  .translate([width / 2, height / 2]);
+
+var path;
+var paths;
+
+var info = svg.append('text')
+  .attr('width',80)
+  .attr('height',20)
+  .attr('y',10)
+  .attr('fill','black');
 
 
-const deps = svg.append("g");
+//   d3.json('swiss-maps.json').then(function(geojson) {
+//     //dessiner le path avec les features des données
+//         deps.selectAll("path")
+//           .data(geojson.features)
+//           .enter()
+//           .append("path")
+//           .attr("d", path);
+//       });
 
-d3.json('../data/swiss-maps.json').then(function(geojson) {
 
-//dessiner le path avec les features des données
-    deps.selectAll("path")
-      .data(geojson.features)
-      .enter()
-      .append("path")
-      .attr("d", path);
-  });
+// let projection = d3.geoNaturalEarth1()
+// .scale(width / 1.8 / Math.PI)
+// .translate([width / 2, height / 2])
+
+// let path = d3.geoPath().projection(projection)
+
+
+
+// d3.json("swiss-maps.json", function(error, dat) {
+//   console.log(dat);
+//   var subunits = topojson.feature(dat, dat.objects.prefs_jpn);
+//   path = d3.geo.path().projection(proj);
+      
+//   g = svg.append('g');
+//   var cities = g.selectAll(".city")
+//     .data(subunits.features)
+//     .enter().append("path")
+//     .attr("class", function(d) { return "subunit " + d.id; })
+//     .attr("d", path);
+
+//     cities.on('click',clicked);
+
+//   });
+
+// function clicked(d) {
+//   var x, y, k;
+
+//   if (d && centered !== d) {
+//     var centroid = path.centroid(d);
+//     x = centroid[0];
+//     y = centroid[1];
+//     k = 4;
+//     centered = d;
+//   } else {
+//     x = width / 2;
+//     y = height / 2;
+//     k = 1;
+//     centered = null;
+//   }
+
+//   g.selectAll("path")
+//     .classed("active", centered && function(d) { return d === centered; });
+
+//   g.transition()
+//     .duration(750)
+//     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+//     .style("stroke-width", (0.5 / k) + "px");
+// }
+
+
+
+
+
+
+
+
+
+// //Pour dire que l'on veut une carte
+// const path = d3.geoPath();
+
+// //Pour définir le centre de la carte
+// const projection = d3.geoConicConformal()
+//   .center([2.454071, 46.279229])
+//   .scale(1000) //Pour définir la couverture
+//   .translate([width / 2, height / 2]);
+
+//   path.projection(projection); //associer le path à la projection
+
+// const svg = d3.select("#graph-loyer-moyen").append("svg")
+//   .attr("id", "svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+//   .attr("class", "Blues");
+
+
+// const deps = svg.append("g");
+
+// d3.json('swiss-maps.json').then(function(geojson) {
+
+// //dessiner le path avec les features des données
+//     deps.selectAll("path")
+//       .data(geojson.features)
+//       .enter()
+//       .append("path")
+//       .attr("d", path);
+//   });
+
+
+
+
+
+
+
+
 
 
 
